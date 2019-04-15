912405ec5e2a5b5ce9427df089edd91fefd2e267
Erik Vorkink <erik@vorkink.com>
2019-04-15 17:38:08 -0600

    Initial Commit

---
+++ GottaDo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GottaDo
+//
+//  Created by Erik Vorkink on 4/15/19.
+//  Copyright © 2019 Erik Vorkink. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GottaDo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GottaDo
+//
+//  Created by Erik Vorkink on 4/15/19.
+//  Copyright © 2019 Erik Vorkink. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

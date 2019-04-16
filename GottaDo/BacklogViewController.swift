c4efd317f40997072b8549c495faa9188b65f51c
Erik Vorkink <erik@vorkink.com>
2019-04-15 22:21:07 -0600

    Tasks are now stored persistently using CoreDate. Only the name property has a value.

--- GottaDo/BacklogViewController.swift
+++ GottaDo/BacklogViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
+import CoreData
 
 class BacklogViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var tasks: [String] = []
+    var tasks: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,33 +12,71 @@ class BacklogViewController: UIViewController {
         title = "Backlog"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Task")
+        
+        do {
+            tasks = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+
 
     @IBAction func addTask(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "New Task",
                                       message: "Add a new task",
                                       preferredStyle: .alert)
         
-        let saveAction = UIAlertAction(title: "Save",
-                                       style: .default) {
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
             [unowned self] action in
             
             guard let textField = alert.textFields?.first,
-                let taskToSave = textField.text else {
+                let newTaskName = textField.text else {
                     return
             }
             
-            self.tasks.append(taskToSave)
+            self.createTask(name: newTaskName)
             self.tableView.reloadData()
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel",
-                                         style: .cancel)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         alert.addTextField()
         
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
         
         present(alert, animated: true)
+    }
+    
+    func createTask(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: managedContext)!
+        
+        let task = NSManagedObject(entity: entity, insertInto: managedContext)
+        task.setValue(name, forKeyPath: "name")
+//        task.setValue(xxxxxxx, forKeyPath: "createdDate")
+//        task.setValue(false, forKeypath: "completed")
+//        task.setValue(false, forKeypath: "deleted")
+        
+        do {
+            try managedContext.save()
+            tasks.append(task)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
 }
 
@@ -49,8 +88,9 @@ extension BacklogViewController: UITableViewDataSource {
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let task = tasks[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = tasks[indexPath.row]
+        cell.textLabel?.text = task.value(forKeyPath: "name") as? String
         return cell
     }
 }

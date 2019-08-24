6b9b0f189bcedb01bbcd8fc183923af49ff7bdd9
Erik Vorkink <erik@vorkink.com>
2019-08-24 13:48:46 -0600

    Set a max width on the task name add/edit field. No strong reason for this...

--- GottaDo/TaskEdit/TaskNameField.swift
+++ GottaDo/TaskEdit/TaskNameField.swift
@@ -1,10 +1,17 @@
 import UIKit
 
 class TaskNameField: UITextField {
+    
+    var maxLength: Int?
 
+    override func awakeFromNib() {
+        delegate = self
+    }
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         initAppearance()
+        self.maxLength = 75
     }
     
     func initAppearance() {
@@ -24,5 +31,15 @@ class TaskNameField: UITextField {
     
     func isValidText() -> Bool {
         return self.getTrimmedText().count > 0
+    }
+}
+
+extension TaskNameField: UITextFieldDelegate {
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if self.maxLength == nil { return true }
+        guard let text = textField.text else { return true }
+        
+        let newLength = text.count + string.count - range.length
+        return newLength <= self.maxLength!
     }
 }
